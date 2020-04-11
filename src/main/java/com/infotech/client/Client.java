84c9d12d0615b19e87b70bf085dc40239cfdca25
anoop.asiwal@gmail.com <Paroo2123#>
2020-04-11 17:25:07 +0530

    HibernateSessionFactoryCreation initial commit

---
+++ src/main/java/com/infotech/client/Client.java
@@ -0,0 +1,18 @@
+package com.infotech.client;
+
+import com.infotech.util.HibernateUtil;
+import org.hibernate.Session;
+
+public class Client {
+    public static void main(String[] args) {
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+            String SQL ="SELECT VERSION()";
+            String result = (String) session.createNativeQuery(SQL).getSingleResult();
+            System.out.println("MYSQL Version is :: ");
+            System.out.println(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
